8e55bf9a3eb0efbf1aff60dfe21cdb6751af4330
datamaning <george_xl@163.com>
2017-01-14 10:38:31 +0800

    fix media.py

--- Project_01/media.py
+++ Project_01/media.py
@@ -1,5 +1,13 @@
 import webbrowser
 class Movie():
+    """Movie in the entertainment center.
+
+    Attributes:
+        title:Movie Title
+        poster_image_url:URL TO movie poster image
+        trailer_youtube_url:Url to trailer on Youtube
+        director:Movie director name
+    """
     def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
         self.title=movie_title
         self.storyline=movie_storyline
